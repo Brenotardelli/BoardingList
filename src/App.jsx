a710d842a010c0b932e49bc806b9174575a0d152
Breno Tardelli <brenot.dev@gmail.com>
2025-04-18 18:48:26 +0200

    delete item from the list added

--- src/App.jsx
+++ src/App.jsx
@@ -11,14 +11,22 @@ const App = () => {
   function handleItems(item) {
     setItems((items) => [...items, item]);
   }
+
+  function handleDeleteItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
+  function handleToggleItem(id) {
+    setItems((items) => items.map((item) => (item.id === id ? { ...item, packed: !item.packed } : item)));
+  }
   return (
     <div className="app">
       <Logo />
       <Form onAddItems={handleItems} />
-      <PackingList items={items} />
+      <PackingList items={items} handleDeleteItem={handleDeleteItem} handleToggleItem={handleToggleItem} />
       <Stats />
     </div>
   );
 };
-
+``
 export default App;
--- src/PackingList.jsx
+++ src/PackingList.jsx
@@ -1,22 +1,18 @@
 import React from "react";
 
-const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: false },
-  { id: 3, description: "Charger", quantity: 1, packed: true },
-];
-
-
-const PackingList = ({ items }) => {
+const PackingList = ({ items, handleDeleteItem, handleToggleItem }) => {
   return (
     <div className="list">
       <ul>
         {items.map(({ id, description, quantity, packed }) => (
           <li key={id}>
+            <input type="checkbox" value={packed} onChange={() => handleToggleItem(id)} />
             <span style={packed ? { textDecorationLine: "line-through" } : {}}>
               {quantity} {description}
             </span>
-            <button className="pack-btn">𝘅</button>
+            <button onClick={() => handleDeleteItem(id)} className="pack-btn">
+              𝘅
+            </button>
           </li>
         ))}
       </ul>
